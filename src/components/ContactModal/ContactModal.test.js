42ef50372f1187278020e3e10515aed5f0eaac89
csdear <csdear138@gmail.com>
2022-02-17 13:40:02 -0600

    better descript for first test

--- src/components/ContactModal/ContactModal.test.js
+++ src/components/ContactModal/ContactModal.test.js
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import { ContactModal } from "./";
 import "@testing-library/jest-dom";
 
-test("renders", async () => {
+test("ContactModal has text 'I am the contact modal'", async () => {
   render(<ContactModal />);
   const text = await screen.findByText("I am the contact modal");
   expect(text).toBeInTheDocument();
