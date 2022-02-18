f3021f182aeb77249b9b462b43958c5cd6c6b0b4
csdear <csdear138@gmail.com>
2022-02-18 10:11:56 -0600

    progress

--- src/components/ContactModal/ContactModal.js
+++ src/components/ContactModal/ContactModal.js
@@ -5,42 +5,81 @@ export const ContactModal = () => {
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
+
+  const [nameError, setNameError] = useState("");
+  const [phoneError, setPhoneError] = useState("");
+  const [emailError, setEmailError] = useState("");
+
   const [isValid, setIsValid] = useState(false);
 
   // run everytime deps name,phone,email change
   // tests if exists and is regex valid.
   useEffect(() => {
-    setIsValid(
-      // console.log("EMAIL>>>>>", email),
-      !!name &&
-        !!phone &&
-        !!email &&
-        /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/.test(phone) &&
-        !/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email)
-    );
+    // when we run first reset all the errors
+    setNameError("");
+    setPhoneError("");
+    setEmailError("");
+
+    // _valid Arrow IIFE
+    // Returns true or false. And sets potential error messages.
+
+    let _valid = (() => {
+      if (!name) {
+        setNameError("Name is required");
+        return false;
+      } else if (!phone) {
+        setPhoneError("Phone is required");
+        return false;
+      } else if (!email) {
+        setEmailError("Email is required");
+        return false;
+      } else if (!/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/.test(phone)) {
+        setPhoneError("Phone is improperly formatted");
+        return false;
+      } else if (!/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+        setEmailError("Email is improperly formatted");
+        return false;
+      } else {
+        // if all of the above pass, return true.
+        return true;
+      }
+    })();
+
+    setIsValid(_valid);
   }, [name, phone, email]);
 
   return (
     <div className={styles.main}>
-      <form>
+      <form
+        onSubmit={() => {
+          e.preventDefault();
+          if (isValid) {
+            submit();
+          } else {
+          }
+        }}
+      >
         <input
           required
           placeholder="Name"
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
+        {!!nameError && <div className={styles.error}>{nameError}</div>}
         <input
           required
           placeholder="Phone Number"
           value={phone}
           onChange={(e) => setPhone(e.target.value)}
         />
+        {!!phoneError && <div className={styles.error}>{phoneError}</div>}
         <input
           required
           placeholder="Email Address"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
+        {!!emailError && <div className={styles.error}>{emailError}</div>}
         <button disabled={!isValid}>Submit</button>
       </form>
     </div>
--- src/components/ContactModal/ContactModal.test.js
+++ src/components/ContactModal/ContactModal.test.js
@@ -1,4 +1,10 @@
-import { render, screen, cleanup, fireEvent } from "@testing-library/react";
+import {
+  render,
+  screen,
+  cleanup,
+  fireEvent,
+  getByText,
+} from "@testing-library/react";
 import { ContactModal } from "./";
 import "@testing-library/jest-dom";
 
@@ -49,20 +55,65 @@ test("Enables submit button once form is valid", () => {
   expect(submitButton).not.toBeDisabled();
 });
 
-// test("Disables submit button when fields are invalid", () => {
-//     render(<ContactModal />);
-//     // Setup, ref the elements
-//     const nameInput = screen.queryByPlaceholderText("Name");
-//     const phoneInput = screen.queryByPlaceholderText("Phone Number");
-//     const emailInput = screen.queryByPlaceholderText("Email Address");
-//     const submitButton = screen.getByText("Submit");
+test("Disables submit button when fields are invalid", () => {
+  render(<ContactModal />);
 
-//     // mock a onChange event
-//     fireEvent.change(nameInput, { target: { value: "Port Exe" } });
-//     fireEvent.change(phoneInput, { target: { value: "123-456-7890" } });
-//     fireEvent.change(emailInput, {
-//       target: { value: "portexeofficial" },
-//     });
+  const nameInput = screen.getByPlaceholderText("Name");
+  const phoneInput = screen.getByPlaceholderText("Phone Number");
+  const emailInput = screen.getByPlaceholderText("Email Address");
+  const submitButton = screen.getByText("Submit");
 
-//     expect(submitButton).not.toBeDisabled();
-//   });
+  fireEvent.change(nameInput, { target: { value: "Port Exe" } });
+  fireEvent.change(phoneInput, {
+    target: { value: "123-456-7890" },
+  });
+  fireEvent.change(emailInput, {
+    target: { value: "portexeofficial" },
+  });
+
+  expect(submitButton).toBeDisabled();
+
+  fireEvent.change(emailInput, {
+    target: { value: "portexeofficial@gmail.com" },
+  });
+
+  expect(submitButton).not.toBeDisabled();
+
+  fireEvent.change(phoneInput, { target: { value: "1234567890" } });
+
+  expect(submitButton).toBeDisabled();
+});
+
+test("Displays error messages for invalid inputs", () => {
+  render(<ContactModal />);
+
+  const nameInput = screen.getByPlaceholderText("Name");
+  const phoneInput = screen.getByPlaceholderText("Phone Number");
+  const emailInput = screen.getByPlaceholderText("Email Address");
+
+  fireEvent.change(nameInput, { target: { value: "Port Exe" } });
+  fireEvent.change(phoneInput, {
+    target: { value: "123-456-7890" },
+  });
+  fireEvent.change(emailInput, {
+    target: { value: "portexeofficial" },
+  });
+
+  let errorDiv = screen.queryByTestId("error");
+  expect(errorDiv).toHaveTextContent("Email is improperly formatted");
+
+  fireEvent.change(phoneInput, { target: { value: "1234567890" } });
+  fireEvent.change(emailInput, {
+    target: { value: "portexeofficial@gmail.com" },
+  });
+
+  errorDiv = screen.queryByTestId("error");
+  expect(errorDiv).toHaveTextContent("Phone is improperly formatted");
+
+  fireEvent.change(phoneInput, {
+    target: { value: "123-456-7890" },
+  });
+
+  errorDiv = screen.queryByTestId("error");
+  expect(errorDiv).not.toBeInTheDocument();
+});
