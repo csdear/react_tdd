4acdbe9192b1c65eef23bd2f6be9e0425c6ef631
csdear <csdear138@gmail.com>
2022-02-27 07:29:35 -0600

    renders list of contacts

---
+++ src/components/ContactList/ContactList.test.js
@@ -0,0 +1,32 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import { ContactList } from './ContactList';
+
+test('Renders list of contacts', () => {
+  const contacts = [
+    {
+      id: 0,
+      name: 'Joe',
+      email: 'test123@gmail.com',
+      phone: '987-654-3210',
+    },
+    {
+      id: 1,
+      name: 'Bob',
+      email: 'test321@gmail.com',
+      phone: '123-456-7890',
+    },
+  ];
+
+  render(<ContactList contacts={contacts} />);
+
+  const joeRow = screen.getByTestId('contact-0');
+  const bobRow = screen.getByTestId('contact-1');
+
+  expect(joeRow).toHaveTextContent('Joe');
+  expect(joeRow).toHaveTextContent('test123@gmail.com');
+  expect(joeRow).toHaveTextContent('987-654-3210');
+
+  expect(bobRow).toHaveTextContent('Bob');
+  expect(bobRow).toHaveTextContent('test321@gmail.com');
+  expect(bobRow).toHaveTextContent('123-456-7890');
+});
